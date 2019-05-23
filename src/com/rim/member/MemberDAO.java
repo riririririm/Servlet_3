e65ce4c4f66ccf5b3cb20a1c52d69a934b17f137
riririririm <48780849+riririririm@users.noreply.github.com>
2019-05-23 12:14:31 +0900

    set

---
+++ src/com/rim/member/MemberDAO.java
@@ -0,0 +1,105 @@
+package com.rim.member;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.rim.util.DBConnector;
+
+public class MemberDAO {
+	public int idCheck(String id) throws Exception{
+		int result=0;
+		
+		Connection conn = DBConnector.getConnection();
+		String sql = "select id from member where id=?";
+		PreparedStatement pst = conn.prepareStatement(sql);
+		pst.setString(1, id);
+		
+		ResultSet rs=pst.executeQuery();
+		
+		if(rs.next()) {
+			result=1;
+		}
+		
+		DBConnector.disConnect(rs,pst, conn);
+		
+		return result;
+	}
+
+	public MemberDTO memberLogin(MemberDTO dto) throws Exception{
+		MemberDTO m = null;
+		
+		Connection conn  = DBConnector.getConnection();
+		String sql="select * from member where id=? and pw=?";
+		PreparedStatement pst = conn.prepareStatement(sql);
+		pst.setString(1, dto.getId());
+		pst.setString(2, dto.getPw());
+		ResultSet rs = pst.executeQuery();
+		
+		if(rs.next()) {
+			m = new MemberDTO();
+			m.setId(rs.getString("id"));
+			m.setPw(rs.getString("pw"));
+			m.setName(rs.getString("name"));
+			m.setPhone(rs.getString("phone"));
+			m.setEmail(rs.getString("email"));
+			m.setAge(rs.getInt("age"));
+		}
+		
+		DBConnector.disConnect(pst, conn);
+		
+		return m;
+	}
+	
+	public int memberJoin(MemberDTO dto) throws Exception{
+		int result=0;
+		Connection conn  = DBConnector.getConnection();
+		String sql="insert into member values(?,?,?,?,?,?)";
+		PreparedStatement pst = conn.prepareStatement(sql);
+		pst.setString(1, dto.getId());
+		pst.setString(2, dto.getPw());
+		pst.setString(3, dto.getName());
+		pst.setString(4, dto.getPhone());
+		pst.setString(5, dto.getEmail());
+		pst.setInt(6, dto.getAge());
+		
+		result = pst.executeUpdate();
+		
+		DBConnector.disConnect(pst, conn);
+		
+		return result;
+	}
+	
+	public int memberUpdate(MemberDTO dto) throws Exception{
+		int result=0;
+		Connection conn  = DBConnector.getConnection();
+		String sql="update member set name=?, pw=?, phone=?, email=?, age=? where id=?";
+		PreparedStatement pst = conn.prepareStatement(sql);
+		pst.setString(6, dto.getId());
+		pst.setString(2, dto.getPw());
+		pst.setString(1, dto.getName());
+		pst.setString(3, dto.getPhone());
+		pst.setString(4, dto.getEmail());
+		pst.setInt(5, dto.getAge());
+		
+		result = pst.executeUpdate();
+		
+		DBConnector.disConnect(pst, conn);
+		
+		return result;
+	}
+	
+	public int memberDelete(String id) throws Exception{
+		int result=0;
+		Connection conn  = DBConnector.getConnection();
+		String sql="delete member where id=?";
+		PreparedStatement pst = conn.prepareStatement(sql);
+		pst.setString(1, id);
+	
+		result = pst.executeUpdate();
+		
+		DBConnector.disConnect(pst, conn);
+		
+		return result;
+	}
+}
